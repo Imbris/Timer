9d6308de2de9fdbfca53f824809b7e3dba47335f
d-wine <d.p.vainstein@gmail.com>
2020-06-01 12:39:28 +0300

    Tests. Starter Pack

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using timer;
+
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private bool status;
+        private object locker = new object();
+
+        [TestInitialize]
+        public void Init()
+        {
+            status = true;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Thread thread = new Thread(() =>
+            {
+                lock (locker)
+                {
+                    Form1 form = new Form1();
+                    try
+                    {
+                        form.Show();
+                        Assert.IsTrue(form.Visible);
+                        form.Close();
+                        Assert.IsFalse(form.Visible);
+                    }
+                    catch (Exception exc)
+                    {
+                        status = false;
+                    }
+                }
+            });
+        }
+    }
+}
