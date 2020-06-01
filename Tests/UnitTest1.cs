b4f17a91009e11a543d79e35317ade96d1d49ae8
d-wine <d.p.vainstein@gmail.com>
2020-06-01 16:52:13 +0300

    Test Are OK but they SHOULDN'T!

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -40,6 +40,36 @@ namespace Tests
                     }
                 }
             });
+            thread.Start();
+            Thread.Sleep(1000);
+        }
+
+        [TestMethod]
+        public void TestConvertToInt()
+        {
+            Thread thread = new Thread(() =>
+            {
+                lock (locker)
+                {
+                    Form1 form = new Form1();
+                    try
+                    {
+                        form.Show();
+                        Assert.AreEqual(form.convert_to_int("4", "абракадабра"), 4);
+                        Assert.AreEqual(form.convert_to_int("ыаыпц", "абракадабра"), 0);
+                        Assert.AreEqual(form.convert_to_int("634 орлор", "абракадабра"), 0);
+                        Assert.AreEqual(form.convert_to_int("632", "абракадабра"), 632);
+                        Assert.AreEqual(form.convert_to_int("632l2krj34", "абракадабра"), 632);
+                        form.Close();
+                   }
+                    catch (Exception exc)
+                    {
+                        status = false;
+                    }
+                }
+            });
+            thread.Start();
+            Thread.Sleep(5000);
         }
     }
 }
